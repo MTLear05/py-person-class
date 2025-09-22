b58d1b695650128c5b279f722a65856cf91fab11
Tarasov Maksym <maksimmm45555@gmail.com>
2025-09-22 18:06:51 +0300

    solved py-person-class task in 30-35 minutes, used some help from chatGPT

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people: dict[str, "Person"] = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    persons = []
+    for human in people:
+        person = Person(human["name"], human["age"])
+        persons.append(person)
+
+    for human in people:
+        person = Person.people[human["name"]]
+        if "wife" in human and human["wife"] is not None:
+            person.wife = Person.people[human["wife"]]
+        if "husband" in human and human["husband"] is not None:
+            person.husband = Person.people[human["husband"]]
+
+    return persons
